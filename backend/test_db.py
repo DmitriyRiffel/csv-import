bedfbebd2e6b67b9d703b508eb876eb00f1ba9e7
Dmitriy Riffel <dmitriyriffel1@gmail.com>
2025-11-21 13:51:28 +0100

    first commit

---
+++ backend/test_db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT version();"))
+    print(result.scalar_one())
